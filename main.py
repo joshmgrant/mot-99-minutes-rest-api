ba834f720532ba8f8f41c4b593bae032f488b00b
joshgrant <joshua.m.grant@gmail.com>
2021-04-05 14:58:53 -0400

    using app endpoints, in-memory data

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# v1 - just an endpoint
+
 @app.route("/sauce_logs")
 def index():
     return "Sauce Logs - Logging your hot sauces"
+
+# keeping data in memory, aka The Very Bad, No Good Idea
+entries = []
+
+# v2 - create POST (add entry) and GET (list all entries) endpoints
+@app.route("/entry", methods=['POST'])
+def add_log():
+    entry = request.form
+    entries.append(entry)
+    return jsonify({'added new sauce log': 'ok'})
+
+@app.route("/entries", methods=['GET'])
+def get_log():
+    return jsonify({"entries": entries})
