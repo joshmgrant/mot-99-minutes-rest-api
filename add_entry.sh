c0dd609953117bbc00ff65a278fd7956c1c4a7be
joshgrant <joshua.m.grant@gmail.com>
2021-04-05 15:15:53 -0400

    api with restful and database layer

--- add_entry.sh
+++ add_entry.sh
@@ -1,3 +1,3 @@
-curl -X POST -d '{"title": "Caliente Cayenne", "description" : "Classic red hot sauce", "heat_level": "medium"}' http://localhost:5000/entry
-curl -X POST -d '{"title": "Jumping Jalapeno", "description" : "Slightly hot green sauce", "heat_level": "mild"}' http://localhost:5000/entry
-curl -X POST -d '{"title": "Great Ghost Pepper", "description": "Intense spicy sauce", "heat_level": "hot"}' http://localhost:5000/entry
+curl -X POST -H "Content-Type: application/json" -d '{"title": "Caliente Cayenne", "description" : "Classic red hot sauce", "heat_level": "medium"}' http://localhost:5000/entries
+curl -X POST -H "Content-Type: application/json" -d '{"title": "Jumping Jalapeno", "description" : "Slightly hot green sauce", "heat_level": "mild"}' http://localhost:5000/entries
+curl -X POST -H "Content-Type: application/json" -d '{"title": "Great Ghost Pepper", "description": "Intense spicy sauce", "heat_level": "hot"}' http://localhost:5000/entries
--- main.py
+++ main.py
@@ -1,30 +1,77 @@
 from flask import Flask, request
-from flask.json import jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///local.db'
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 api = Api(app)
 
 
-# v1 - just an endpoint
 @app.route("/sauce_logs")
 def index():
     return "Sauce Logs - Logging your hot sauces"
 
-entries = []
+# v4 - add database instead of using in-memory storage
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    description = db.Column(db.String(255))
+    heat_level = db.Column(db.String(10))
+
+    def __repr__(self):
+        return '<Entry {}>'.format(self.title)
+
+
+class EntrySchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "description", "heat_level")
+
+
+entry_schema = EntrySchema()
+entries_schema = EntrySchema(many=True)
 
 class EntriesResource(Resource):
     def get(self):
-        return jsonify({"entries": entries})
+        entries = Entry.query.all()
+        return entries_schema.dump(entries)
 
+    def post(self):
+        new_entry = Entry(
+            title = request.json['title'],
+            description = request.json['description'],
+            heat_level = request.json['heat_level']
+        )
+        db.session.add(new_entry)
+        db.session.commit()
+        return entry_schema.dump(new_entry)
 
 class EntryResource(Resource):
-    def post(self):
-        entry = request.form
-        entries.append(entry)
-        return jsonify({"added new sauce log", "ok"})
+    def get(self, post_id):
+        entry = Entry.query.get_or_404(post_id)
+        return entry_schema.dump(entry)
+
+    def patch(self, post_id):
+        entry = Entry.query.get_or_404(post_id)
+
+        if 'title' in request.json:
+            entry.title = request.json['title']
+        if 'description' in request.json:
+            entry.description = request.json['description']
+        if 'heat_level' in request.json:
+            entry.heat_level = request.json['heat_level']
+
+        db.session.commit()
+        return entry_schema.dump(entry)
+
+    def delete(self, post_id):
+        entry = Entry.query.get_or_404(post_id)
+        db.session.delete(entry)
+        db.session.commit()
+        return '', 204
 
 
 api.add_resource(EntriesResource, '/entries')
-api.add_resource(EntryResource, '/entry')
+api.add_resource(EntryResource, '/entry/<int:post_id>')
