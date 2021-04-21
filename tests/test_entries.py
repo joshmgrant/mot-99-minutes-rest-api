a04b1fdb8185be8dc74e4e54ae0cbb0e6ba3b387
joshgrant <joshua.m.grant@gmail.com>
2021-04-21 09:49:03 -0400

    added parametrized test

--- tests/test_entries.py
+++ tests/test_entries.py
@@ -1,14 +1,35 @@
+import pytest
+
+
+post_testdata = [{
+        'title': "A Sauce",
+        'description': 'Some kind of hot sauce',
+        'heat_level': 'medium'
+    },{
+        'title': "Sauce with No Description",
+        'description': '',
+        'heat_level': 'mild'
+    }, {
+        'title': "Sauce with No Heat Level",
+        'description': 'A pretty good sauce',
+        'heat_level': ''
+    }, {
+        'title': "Sauce with Nothing",
+        'description': '',
+        'heat_level': ''
+    }, {
+        'title': "",
+        'description': '',
+        'heat_level': ''
+}]
+
 def test_entries_get(saucelog):
     response = saucelog.get("/entries")
 
     assert response.status_code_is(200)
 
-def test_entries_post(saucelog):
-    example_post = {
-        'title': "A Sauce",
-        'description': 'Some kind of hot sauce',
-        'heat_level': 'medium'
-    }
+@pytest.mark.parametrize("example_post", post_testdata)
+def test_entries_post(saucelog, example_post):
 
     response = saucelog.post("/entries", data=example_post)
 
